6608fb03dc5e42ea9ed9386f3ed1800868d3f742
lc316406448 <72664686+lc316406448@users.noreply.github.com>
2024-12-17 16:45:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { connect } from 'cloudflare:sockets';
  * User configuration and settings
  * Generate UUID: [Windows] Press "Win + R", input cmd and run: Powershell -NoExit -Command "[guid]::NewGuid()"
  */
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '19b6703c-1080-4d3f-b364-aed1163c1303';
 
 /**
  * Array of proxy server addresses with ports
